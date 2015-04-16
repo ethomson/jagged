e7444993c4bda544ea9549bf3f5355ac1c68c730
Carlos Martín Nieto <cmn@dwim.me>
2015-04-16 21:36:25 +0200

    Update to libgit2 0.22.2
    The only thing that changed which jagged relies on is the global
    initialisation, so the diff ends up being pretty small.

--- src/test/java/org/libgit2/jagged/VersionTest.java
+++ src/test/java/org/libgit2/jagged/VersionTest.java
@@ -22,7 +22,7 @@ public class VersionTest
         Version libGit2Version = Version.getLibGit2Version();
 
         Assert.assertEquals(0, libGit2Version.getMajor());
-        Assert.assertEquals(21, libGit2Version.getMinor());
-        Assert.assertEquals(3, libGit2Version.getRevision());
+        Assert.assertEquals(22, libGit2Version.getMinor());
+        Assert.assertEquals(2, libGit2Version.getRevision());
     }
 }
